7957c9c2a18e7c9106fd17764b4d870f5dcea879
Imane-Bjr <boujirimane1@gmail.com>
2024-06-06 13:09:32 -0700

    0. New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,10 +17,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (!my_node)
 		return (NULL);
 
-	new_node->parent = parent;
-	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
+	my_node->parent = parent;
+	my_node->n = value;
+	my_node->left = NULL;
+	my_node->right = NULL;
 
 	return (my_node);
 }
