cb4687d228e930f2ad07ced6c301371189eb14e3
Imane-Bjr <boujirimane1@gmail.com>
2024-06-06 23:01:32 +0100

    2. Insert right + 3. Delete

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - Inserts a node as the left-child of another node
+ * binary_tree_insert_right - Inserts a node as the right-child of another node
  * @parent: Pointer to the parent node
  * @value: Value to store in the new node
  *
  * Return: Pointer to the created node, or NULL on failure or if parent is NULL
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *my_node;
 
@@ -17,12 +17,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	my_node = binary_tree_node(parent, value);
 	if (!my_node)
 		return (NULL);
-	if (parent->left != NULL)
+	if (parent->right != NULL)
 	{
-		my_node->left = parent->left;
-		parent->left->parent = my_node;
+		my_node->right= parent->right;
+		parent->right->parent = my_node;
 	}
 
-	parent->left = my_node;
+	parent->right = my_node;
 	return (my_node);
 }
