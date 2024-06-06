175c8a223048c91a80f39e07a5efd8a4b304ba59
Imane-Bjr <boujirimane1@gmail.com>
2024-06-06 16:20:51 -0700

    9. Height + 10. Depth

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,18 +8,20 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_h = 0;
-	size_t right_h = 0;
+	size_t left_h = 0, right_h = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left_h = binary_tree_height(tree->left);
-	right_h = binary_tree_height(tree->right);
+	if (tree->left)
+		left_h = 1 + binary_tree_height(tree->left);
 
-	if (left_h > right_h)
-		return (left_h + 1);
+	if (tree->right)
+		right_h = 1 + binary_tree_height(tree->right);
+
+	if (right_h > left_h)
+		return (right_h);
 	else
-		return (right_h + 1);
+		return (left_h);
 }
 
